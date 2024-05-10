cc5d8515555e0c8ccbc59d346a4382e853d985ee
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2024-05-10 19:42:19 -0400

    setup: cna with tailwind

---
+++ nextjs-postgres-docker/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex bg-zinc-200 min-h-screen flex-col items-center pt-10">
+      <h1 className="text-3xl text-black font-medium">All tasks</h1>
+    </main>
+  );
+}
