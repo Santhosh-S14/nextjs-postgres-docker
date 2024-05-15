452f11252a720d8ec4409db89aa712a33fe623e7
Santhosh S <39198124+Santhosh-S14@users.noreply.github.com>
2024-05-14 20:42:04 -0400

    A simple hardcoded way of showing tasks and a form to add tasks

---
+++ nextjs-postgres-docker/src/app/actions/actions.ts
@@ -0,0 +1,5 @@
+"use server";
+
+export async function addTask(formData: FormData) {
+    console.log("Adding task...")
+}
--- nextjs-postgres-docker/src/app/page.tsx
+++ nextjs-postgres-docker/src/app/page.tsx
@@ -1,7 +1,39 @@
+import { addTask } from "./actions/actions";
+
 export default function Home() {
+  const tasks = [
+    {
+      id: 1,
+      title: "Task 1",
+    },
+    {
+      id: 2,
+      title: "Task 2",
+    },
+  ];
   return (
     <main className="flex bg-zinc-200 min-h-screen flex-col items-center pt-10">
       <h1 className="text-3xl text-black font-medium">All tasks</h1>
+      <ul className="my-10 text-center">
+        {tasks.map((task) => (
+          <li key={task.id} className="text-black">
+            {task.title}
+          </li>
+        ))}
+      </ul>
+      <form action={addTask} className="space-x-2 h-4">
+        <input
+          type="text"
+          name="title"
+          className="px-3 py-1 rounded text-black"
+        ></input>
+        <button
+          type="submit"
+          className="px-3 py-1 rounded text-white bg-blue-500"
+        >
+          Add task
+        </button>
+      </form>
     </main>
   );
 }
